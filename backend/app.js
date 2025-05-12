e9e5986822cc3645363b8cde7006fcda58231a34
Prajualit Tickoo <23bec072@nith.ac.in>
2025-05-12 18:35:58 +0530

    backend

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+export { app };
